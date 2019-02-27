a903cacb9f09755270ea8886335bb29cc2059b50
Ankit-singh72 <Ankit-singh7>
2019-02-28 01:04:25 +0530

    commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Todo Application",
+  "version": "1.0.0",
+  "description": " Documentation for the todo app",
+  "title": "Todo APIs",
+  "url": "http://api.trego.tk",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-02-27T19:28:45.263Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
